f3aebdf0954b5eafcbb7e4c365accd08758edafe
Zhi Han <zhih@fb.com>
2023-06-30 17:02:07 -0400

    Added load document and splitter.

--- main.mjs
+++ main.mjs
@@ -4,8 +4,10 @@ import * as session from './session.mjs'
 
 // Supported commands
 const COMMANDS = new Map([
-  ["load", "Load resources"],
-  ["quit", "Quit the conversation (^D)"],
+  ["load", "Load doc"],
+  ["quit", "Quit (^D)"],
+  ["status", ""],
+  ["answer", "Answer questions based on given facts"]
 ]);
 
 /** Prompt the user and get the answer */
@@ -16,7 +18,7 @@ function question(prompt) {
   });
 
   return new Promise((resolve) => {
-    rl.question(prompt, (answer) => {
+    rl.question(chalk.cyan(prompt), (answer) => {
       rl.close();
       resolve(answer);
     });
@@ -24,11 +26,11 @@ function question(prompt) {
 }
 
 /** Callback for the load command. */
-async function load_resource() {
-  const url = await question('> Input the resource URL:\n');
+async function load_doc() {
+  const url = await question('> Input the document:\n');
   try {
-    await session.load_resource(url);
-    console.log("> Successfully loaded");
+    await session.load_doc(url);
+    console.log(chalk.green("Successfully loaded"));
   } catch (error) {
     console.error("??? Failed to load:" + error);
   }
@@ -40,23 +42,61 @@ async function chat(user_input) {
   console.log(chalk.cyan(`${res}`));
 }
 
+/** Chat based on search result. Retry if no search result found. */
+async function answer() {
+  let docs = [];
+  while (docs.length === 0) {
+    let query = await question('> Input the search query:\n');
+    try {
+      docs = await session.search(query);
+    } catch (error) {
+      console.error("??? Failed to search:" + error);
+    }
+  }
+  const user_input = await question(`> What questions do you have?\n`);
+  try {
+    const res = await session.chat_with_docs(user_input, docs);
+    console.log(chalk.cyan(`${res}`));
+  } catch (error) {
+    console.error("??? Failed:" + error);
+  }
+}
+
 /** Handle user commands. */
 async function handle_command(user_input) {
   const lowered = user_input.toLowerCase();
   if (!COMMANDS.has(lowered)) {
+    const trimmed = user_input.trim();
+    if (trimmed.length === 0) {
+      return true;
+    }
+
     await chat(user_input);
-    return true;
   }
   if (lowered === "load") {
-    await load_resource();
-    return true;
+    await load_doc();
   } else if (lowered == "quit") {
     return false;
+  } else if (lowered == "status") {
+    const status = session.status();
+    console.log(chalk.green(status));
+  } else if (lowered == "answer") {
+    await answer();
   }
+  return true;
+
 }
 
+
+const WELCOME = `
+Welcome to a CLI to ChatGPT.
+You can use me to interact with ChatGPT. In addition, you can load a reference document,
+and I will answer your questions based on that reference.
+
+Start by asking a question, or load a reference document.`
+
 async function main() {
-  console.log(chalk.cyan("Welcome to a CLI to ChatGPT. Start by asking a question"))
+  console.log(chalk.cyan(WELCOME))
   let cont = true;
   while (cont) {
     const hints = Array.from(COMMANDS.entries()).map(
--- session.mjs
+++ session.mjs
@@ -1,38 +1,72 @@
 import chalk from 'chalk'
 import 'dotenv/config'  // Use .env config
-import { OpenAI } from "langchain/llms/openai";
 import { BufferMemory } from "langchain/memory";
 import { ConversationChain } from "langchain/chains";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { OpenAI } from "langchain/llms/openai";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { PromptTemplate } from "langchain/prompts";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { TextLoader } from "langchain/document_loaders/fs/text";
 
 /** Create a session with built-in memory. */
-function new_session() {
+async function new_session() {
   let model = new OpenAI({ temprature: 0.1 })
   let memory = new BufferMemory();
-  let chain = new ConversationChain({ llm: model, memory: memory })
+  let chain = new ConversationChain({ llm: model, memory: memory });
+  let splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 20,
+  });
+  let vector_store = await HNSWLib.fromDocuments([], new OpenAIEmbeddings());
   return {
-    resources: [],
+    documents: [],
     model,
+    splitter,
     memory,
     chain,
+    vector_store,
   };
 }
 
-var session = new_session();
+var session = await new_session();
 
 /** Load resource into langchain */
-export async function load_resource(url) {
-  console.log(`Loading ${url}`);
+export async function load_doc(url) {
   const loader = new TextLoader(url);
   const docs = await loader.load();
-  session.resources.push(docs);
+  const splitted = await session.splitter.splitDocuments(docs);
+  await session.vector_store.addDocuments(splitted);
+  session.documents.push(...splitted);
 }
 
 /** Search the resources and find relevant information. */
-export function search(search_query) {
-  console.log(`searching '${search_query}'`)
-  return ["search result"];
+export async function search(query) {
+  let search_result = await session.vector_store.similaritySearch(query);
+  return search_result;
+}
+
+/** Send the chat to chatGPT and get the answer */
+export async function chat_with_docs(question, docs) {
+  // TODO: save context.
+  const template = `
+  You are a personal assistant. Answer the question based on
+  the result of these content. Respond 'I don't know' if the
+  information is irrelevant.
+  ===
+  {content}
+  ===
+  Question: {question}
+  `
+  const prompt = new PromptTemplate({
+    template: template,
+    inputVariables: ["content", "question"],
+  });
+  const content = docs.map(doc => doc.pageContent).join("\n\n");
+  console.log(chalk.gray("Thinking..."));
+  const input = await prompt.format({ content, question });
+  const res = await session.chain.call({ input });
+  return res.response;
 }
 
 /** Send the chat to chatGPT and get the answer */
@@ -47,4 +81,13 @@ export async function chat(user_input) {
   const input = await prompt.format({ question: user_input });
   const res = await session.chain.call({ input });
   return res.response;
+}
+
+/** Generate a summary of the current session */
+export function status() {
+  if (session.documents.length === 0) {
+    return "no documents";
+  }
+  const docs = session.documents.map(doc => doc.metadata.source).join(", ");
+  return `${session.documents.length} documents: ${docs}`;
 }
